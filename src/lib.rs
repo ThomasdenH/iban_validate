12c28cf5f71874374b5449c63e988e9ea6e06284
Thomas den Hollander <denhollander.thomas@gmail.com>
2017-04-14 22:28:29 +0200

    Initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,73 @@
-pub mod iban;
-
 #[cfg(test)]
 mod tests;
+
+extern crate regex;
+#[macro_use] extern crate lazy_static;
+
+use regex::Regex;
+
+/// Validate an IBAN number. The validation will return false in the following cases:
+/// <ul>
+/// <li>If the length is four or less, or longer than 34.</li>
+/// <li>If the number contains characters other than A-Z or 0-9</li>
+/// <li>If A-Z is in place of 0-9 or vice versa</li>
+/// <li>If the checksum is invalid</li>
+/// </ul>
+///
+/// # Examples
+/// ```rust
+/// use iban::validate_iban;
+///
+/// // A valid address
+/// assert_eq!(validate_iban("DE44500105175407324931"), true);
+///
+/// // An invalid address
+/// assert_eq!(validate_iban("DE4450010234607324931"), false);
+/// ```
+pub fn validate_iban<S: Into<String>>(address: S) -> bool {
+
+    let address_string = address.into();
+
+    return
+        // Check the characters
+        validate_characters(&address_string)
+        // Check the checksum
+        && compute_checksum(&address_string) == 1;
+}
+
+/// Checks whether all characters in this address are valid. Returns a true if all characters are
+/// valid, false otherwise.
+fn validate_characters(address: &String) -> bool {
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"[A-Z]{2}\d{2}[A-Z\d]{1,30}").unwrap();
+    }
+    RE.is_match(address)
+}
+
+/// This function computes the checksum of an address. The function assumes the string only
+/// contains 0-9 and A-Z.
+///
+/// # Panics
+/// If the address contains any characters other than 0-9 or A-Z, this function will panic.
+fn compute_checksum(address: &String) -> u8 {
+    let mut digits = Vec::new();
+
+    let (start, end) = address.split_at(4);
+    let mut changed_order = String::new();
+    changed_order.push_str(end);
+    changed_order.push_str(start);
+
+    for c in changed_order.chars() {
+        match c {
+            d @ '0'...'9' => digits.push(d.to_digit(10).unwrap()),
+            a @ 'A'...'Z' => {
+                let number = a.to_digit(36).unwrap();
+                digits.push(number / 10);
+                digits.push(number % 10);
+            },
+            _ => panic!("Invalid character in address")
+        }
+    }
+
+    digits.iter().fold(0, |acc, d| (acc * 10 + d) % 97) as u8
+}
--- src/tests/mod.rs
+++ src/tests/mod.rs
@@ -1,71 +1,61 @@
-use iban::Iban;
-use iban::IbanValidationResult;
+use validate_iban;
 
 #[test]
 fn test_length() {
-    let a = Iban::new("DE4");
-    assert_eq!(a.validate(), IbanValidationResult::InvalidLength { l: 3 });
+    let invalid_lengths = [
+        "DE4",
+        "DE445001023460732493147896512575467"
+    ];
 
-    let b = Iban::new("DE445001023460732493147896512575467");
-    assert_eq!(b.validate(), IbanValidationResult::InvalidLength { l: 35 });
+    validate_all(&invalid_lengths, false);
 }
 
 #[test]
 fn test_characters() {
-    let a = Iban::new("DE44@0010234607324931");
-    assert_eq!(a.validate(), IbanValidationResult::InvalidCharacter { i: 4, c: '@' });
+    let invalid_characters = [
+        "DE44@0010234607324931",
+        "DE44@0010234607324931",
+        "$A0380000000648510167519",
+        "tr330006100519786457465326",
+        "G416011012500000834112300695",
+        "CHI300762011623852957"
+    ];
 
-    let b = Iban::new("$A0380000000648510167519");
-    assert_eq!(b.validate(), IbanValidationResult::InvalidCharacter { i: 0, c: '$' });
-
-    let c = Iban::new("tr330006100519786457465326");
-    assert_eq!(c.validate(), IbanValidationResult::InvalidCharacter { i: 0, c: 't' });
-
-    let d = Iban::new("G416011012500000834112300695");
-    assert_eq!(d.validate(), IbanValidationResult::InvalidCharacter { i: 1, c: '4' });
-
-    let e = Iban::new("CHI300762011623852957");
-    assert_eq!(e.validate(), IbanValidationResult::InvalidCharacter { i: 2, c: 'I' });
+    validate_all(&invalid_characters, false);
 }
 
 #[test]
 fn test_checksum() {
-    let a = Iban::new("DE4450010234607324931");
-    assert_eq!(a.validate(), IbanValidationResult::InvalidChecksum { s: 69 });
 
-    let b = Iban::new("GR16011012500000834112300695");
-    assert_eq!(b.validate(), IbanValidationResult::InvalidChecksum { s: 5 });
+    let invalid_checksums = [
+        "DE4450010234607324931",
+        "GR16011012500000834112300695",
+        "GB29NWBK60934331926819",
+        "SA0380000000648510167519",
+        "CH9300762011645852957",
+        "TR330006100519786457465326"
+    ];
 
-    let c = Iban::new("GB29NWBK60934331926819");
-    assert_eq!(c.validate(), IbanValidationResult::InvalidChecksum { s: 60 });
-
-    let d = Iban::new("SA0380000000648510167519");
-    assert_eq!(d.validate(), IbanValidationResult::InvalidChecksum { s: 29 });
-
-    let e = Iban::new("CH9300762011645852957");
-    assert_eq!(e.validate(), IbanValidationResult::InvalidChecksum { s: 34 });
-
-    let f = Iban::new("TR330006100519786457465326");
-    assert_eq!(f.validate(), IbanValidationResult::InvalidChecksum { s: 21 });
+    validate_all(&invalid_checksums, false);
 }
 
 #[test]
 fn test_valid_iban() {
-    let a = Iban::new("DE44500105175407324931");
-    assert_eq!(a.validate(), IbanValidationResult::Valid);
 
-    let b = Iban::new("GR1601101250000000012300695");
-    assert_eq!(b.validate(), IbanValidationResult::Valid);
+    let valid_ibans = [
+        "DE44500105175407324931",
+        "GR1601101250000000012300695",
+        "GB29NWBK60161331926819",
+        "SA0380000000608010167519",
+        "CH9300762011623852957",
+        "TR330006100519786457841326"
+    ];
 
-    let c = Iban::new("GB29NWBK60161331926819");
-    assert_eq!(c.validate(), IbanValidationResult::Valid);
+    validate_all(&valid_ibans, true);
+}
 
-    let d = Iban::new("SA0380000000608010167519");
-    assert_eq!(d.validate(), IbanValidationResult::Valid);
-
-    let e = Iban::new("CH9300762011623852957");
-    assert_eq!(e.validate(), IbanValidationResult::Valid);
-
-    let f = Iban::new("TR330006100519786457841326");
-    assert_eq!(f.validate(), IbanValidationResult::Valid);
+fn validate_all(numbers: &[&str], result: bool) {
+    for &n in numbers {
+        assert_eq!(validate_iban(n), result);
+    }
 }
