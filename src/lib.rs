54a94732a92f37ab8bb5680afe3ad2c32eca9805
Thomas den Hollander <denhollander.thomas@gmail.com>
2017-04-16 14:31:48 +0200

    Add new function to validate country codes

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,14 @@
 #[cfg(test)]
 mod tests;
 
+mod countries;
+
 extern crate regex;
 #[macro_use]
 extern crate lazy_static;
 
 use regex::Regex;
+use countries::COUNTRY_FORMATS;
 
 /// Validate an IBAN number. The validation will detect the following mistakes:
 /// <ul>
@@ -15,6 +18,8 @@ use regex::Regex;
 ///     <li>The checksum is invalid</li>
 /// </ul>
 /// If none of these apply, the function will return true, otherwise it will return false.
+/// Note that this function will not check the country format. To validate the country code and
+/// the BBAN format, you should also use `validate_iban_country()`.
 ///
 /// # Examples
 /// ```rust
@@ -75,4 +80,60 @@ fn compute_checksum(address: &String) -> u8 {
 
     // Validate the checksum
     digits.iter().fold(0, |acc, d| (acc * 10 + d) % 97) as u8
+}
+
+/// The three possible results of `validate_iban_country()`.
+#[derive(PartialEq, Eq, Debug)]
+pub enum IbanCountryResult {
+    /// The country was recognized and the code was valid
+    Valid,
+    /// The country was recognized and didn't fit the format
+    Invalid,
+    /// The country was not recognized
+    CountryUnknown,
+}
+
+/// Validate the BBAN part of an IBAN account number. This function will return one of three
+/// results:
+/// <ul>
+///     <li>If the country code is recognized and the address fits the country's format, it will
+///         return `IbanCountryResult::Valid`.</li>
+///     <li>If the country code is recognized and the address does not fit the country BBAN format,
+///         it will return `IbanCountryResult::Invalid`.</li>
+///     <li>If the country code is not recognized, it will return
+///         `IbanCountryResult::CountryUnknown`.</li>
+/// </ul>
+/// Note that this check is not a substitute for `validate_iban()` or vice versa. This function
+/// only checks the address country code and corresponding format. To verify whether the address
+/// fits the IBAN specification, you should also call `validate_iban()`.
+///
+/// # Examples
+/// ```rust
+/// use iban::validate_iban_country;
+/// use iban::IbanCountryResult;
+///
+/// // A valid address format
+/// assert_eq!(validate_iban_country("DE44500105175407324931"), IbanCountryResult::Valid);
+///
+/// // An invalid format
+/// assert_eq!(validate_iban_country("DE44ABCDE5175407324931"), IbanCountryResult::Invalid);
+///
+/// // An unknown country
+/// assert_eq!(validate_iban_country("ZZ44500105175407324931"), IbanCountryResult::CountryUnknown);
+/// ```
+pub fn validate_iban_country<S: Into<String>>(address: S) -> IbanCountryResult {
+    let address_string = address.into();
+    let (country_code_address, address_remainder) = address_string.split_at(2);
+
+    for &(country_code_pattern, country_regex) in COUNTRY_FORMATS.into_iter() {
+        if country_code_pattern == country_code_address {
+            // The country code matches
+            let regex = Regex::new(country_regex).unwrap();
+            return match regex.is_match(address_remainder) {
+                       true => IbanCountryResult::Valid,
+                       false => IbanCountryResult::Invalid,
+                   };
+        }
+    }
+    IbanCountryResult::CountryUnknown
 }
