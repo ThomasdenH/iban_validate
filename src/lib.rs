53aa872a82088dfafa9f76de36c5a18b69a19060
Thomas <denhollander.thomas@gmail.com>
2017-05-08 17:20:15 +0200

    Update crate documentation and structure to better conform to the rust API guidelines

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,26 @@
+/*!
+This crate provides an easy way to validate an IBAN account number. To do this, it has two
+functions, [`validate_iban`] which validates the IBAN format, and [`validate_iban_country`] which
+validates the country specific format of the account number.
+
+[`validate_iban`]: ./fn.validate_iban.html
+[`validate_iban_country`]: ./fn.validate_iban_country.html
+
+# Example
+The following example does a complete validation of the account number:
+
+```rust
+use iban::*;
+
+let account_number = "DE44500105175407324931";
+let valid = validate_iban(account_number) &&
+        validate_iban_country(account_number) == IbanCountryResult::Valid;
+
+assert!(valid);
+```
+
+*/
+
 #[cfg(test)]
 extern crate spectral;
 extern crate regex;
@@ -20,7 +43,9 @@ use countries::COUNTRY_FORMATS;
 /// </ul>
 /// If none of these apply, the function will return true, otherwise it will return false.
 /// Note that this function will not check the country format. To validate the country code and
-/// the BBAN format, you should also use `validate_iban_country()`.
+/// the BBAN format, you should also use [`validate_iban_country`].
+///
+/// [`validate_iban_country`]: ./fn.validate_iban_country.html
 ///
 /// # Examples
 /// ```rust
@@ -32,22 +57,21 @@ use countries::COUNTRY_FORMATS;
 /// // An invalid address
 /// assert_eq!(validate_iban("DE4450010234607324931"), false);
 /// ```
-pub fn validate_iban<S: Into<String>>(address: S) -> bool {
-
-    let address_string = address.into();
-
+pub fn validate_iban(address: &str) -> bool {
     return
         // Check the characters
-        validate_characters(&address_string)
+        validate_characters(&address)
         // Check the checksum
-        && compute_checksum(&address_string) == 1;
+        && compute_checksum(&address) == 1;
 }
 
 /// Checks whether all characters in this address are valid. Returns a true if all characters are
 /// valid, false otherwise.
-fn validate_characters(address: &String) -> bool {
+fn validate_characters(address: &str) -> bool {
     lazy_static! {
-        static ref RE: Regex = Regex::new(r"^[A-Z]{2}\d{2}[A-Z\d]{1,30}$").unwrap();
+        static ref RE: Regex = Regex::new(r"^[A-Z]{2}\d{2}[A-Z\d]{1,30}$")
+            .expect("Could not compile regular expression. Please file an issue at \
+                https://github.com/ThomasdenH/iban_validate.");
     }
     RE.is_match(address)
 }
@@ -57,7 +81,7 @@ fn validate_characters(address: &String) -> bool {
 ///
 /// # Panics
 /// If the address contains any characters other than 0-9 or A-Z, this function will panic.
-fn compute_checksum(address: &String) -> u8 {
+fn compute_checksum(address: &str) -> u8 {
     address.chars()
         // Move the first four characters to the back
         .cycle()
@@ -66,7 +90,9 @@ fn compute_checksum(address: &String) -> u8 {
         // Calculate the checksum
         .fold(0, |acc, c| {
             // Convert '0'-'Z' to 0-35
-            let digit = c.to_digit(36).unwrap();
+            let digit = c.to_digit(36)
+                .expect("An address was supplied to compute_checksum with an invalid character. \
+                    Please file an issue at https://github.com/ThomasdenH/iban_validate.");
             // If the number consists of two digits, multiply by 100
             let multiplier = if digit > 9 { 100 } else { 10 };
             // Calculate modulo
@@ -74,8 +100,11 @@ fn compute_checksum(address: &String) -> u8 {
         }) as u8
 }
 
-/// The three possible results of `validate_iban_country()`.
-#[derive(PartialEq, Eq, Debug)]
+/// The function [`validate_iban_country`] will return a variant of this enum.
+///
+/// [`validate_iban_country`]: ./fn.validate_iban_country.html
+///
+#[derive(Copy, Clone, Eq, PartialEq, Hash, Debug)]
 pub enum IbanCountryResult {
     /// The country was recognized and the code was valid
     Valid,
@@ -89,15 +118,20 @@ pub enum IbanCountryResult {
 /// results:
 /// <ul>
 ///     <li>If the country code is recognized and the address fits the country's format, it will
-///         return `IbanCountryResult::Valid`.</li>
+///         return [`IbanCountryResult::Valid`].</li>
 ///     <li>If the country code is recognized and the address does not fit the country BBAN format,
-///         it will return `IbanCountryResult::Invalid`.</li>
+///         it will return [`IbanCountryResult::Invalid`].</li>
 ///     <li>If the country code is not recognized, it will return
-///         `IbanCountryResult::CountryUnknown`.</li>
+///         [`IbanCountryResult::CountryUnknown`].</li>
 /// </ul>
-/// Note that this check is not a substitute for `validate_iban()` or vice versa. This function
+/// Note that this check is not a substitute for [`validate_iban`] or vice versa. This function
 /// only checks the address country code and corresponding format. To verify whether the address
-/// fits the IBAN specification, you should also call `validate_iban()`.
+/// fits the IBAN specification, you should also call [`validate_iban`].
+///
+/// [`IbanCountryResult::Valid`]: ./enum.IbanCountryResult.html#variant.Valid
+/// [`IbanCountryResult::Invalid`]: ./enum.IbanCountryResult.html#variant.Invalid
+/// [`IbanCountryResult::CountryUnknown`]: ./enum.IbanCountryResult.html#variant.CountryUnknown
+/// [`validate_iban`]: ./fn.validate_iban.html
 ///
 /// # Examples
 /// ```rust
@@ -113,14 +147,15 @@ pub enum IbanCountryResult {
 /// // An unknown country
 /// assert_eq!(validate_iban_country("ZZ44500105175407324931"), IbanCountryResult::CountryUnknown);
 /// ```
-pub fn validate_iban_country<S: Into<String>>(address: S) -> IbanCountryResult {
-    let address_string = address.into();
-    let (country_code_address, address_remainder) = address_string.split_at(2);
+pub fn validate_iban_country(address: &str) -> IbanCountryResult {
+    let (country_code_address, address_remainder) = address.split_at(2);
 
     for &(country_code_pattern, country_regex) in COUNTRY_FORMATS.into_iter() {
         if country_code_pattern == country_code_address {
             // The country code matches
-            let regex = Regex::new(country_regex).unwrap();
+            let regex = Regex::new(country_regex)
+                .expect("Could not compile regular expression. Please file an issue at \
+                    https://github.com/ThomasdenH/iban_validate.");
             return if regex.is_match(address_remainder) {
                        IbanCountryResult::Valid
                    } else {
