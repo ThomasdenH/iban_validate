e82921742f0ad787ccca92fcd73793e6a7ee7bf0
Thomas den Hollander <denhollander.thomas@gmail.com>
2020-01-22 12:15:00 +0100

    Add benchmark

---
+++ benches/iban_benchmark.rs
@@ -0,0 +1,11 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use iban::Iban;
+use std::str::FromStr;
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let iban_str = "DE44500105175407324931";
+    c.bench_function(iban_str, |b| b.iter(|| Iban::from_str(black_box(iban_str)).unwrap()));
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
