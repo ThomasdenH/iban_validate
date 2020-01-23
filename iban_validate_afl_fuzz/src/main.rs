596ebf66e696a0d2c92fc79f35e392adefec0886
Thomas <denhollander.thomas@gmail.com>
2020-01-23 12:38:27 +0100

    Add afl for fuzzing

---
+++ iban_validate_afl_fuzz/src/main.rs
@@ -0,0 +1,10 @@
+use afl::fuzz;
+use iban::Iban;
+
+fn main() {
+    fuzz!(|data: &[u8]| {
+        if let Ok(s) = std::str::from_utf8(data) {
+            let _ = s.parse::<Iban>();
+        }
+    });
+}
